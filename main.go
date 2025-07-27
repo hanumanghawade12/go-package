b43e51e4e36e99dc1c6eb7626fe9201bbcfced56
Hanuman Ghawade <hanuman.ghawade@sadco.io>
2025-07-27 11:51:35 +0530

    intiial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	// This is a placeholder for the main function.
+	// You can add your code here to execute when the program runs.
+
+	// give to simople method
+	println("Hello, World!")
+	// You can also call other functions or methods here.
+	// For example, you can define and call a function:
+
+}
+func SayHello() {
+	// This function prints a greeting message.
+	println("Hello from sayHello function!")
+}
